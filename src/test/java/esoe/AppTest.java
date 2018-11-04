26fe2eac73a3217a3038d2e73ef9d3e8cc56660e
esoe <igor_nikulin@mail.ru>
2018-11-04 12:22:28 +0300

    библиотека для ведения лога программы

---
+++ src/test/java/esoe/AppTest.java
@@ -0,0 +1,20 @@
+package esoe;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
